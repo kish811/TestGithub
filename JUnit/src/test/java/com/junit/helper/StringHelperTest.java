7b935dc36ce7b35a58a5ed7a888cf71068934aa8
2160731 <2160731@LTIN444067.cts.com>
2023-07-10 15:20:39 +0530

    JUnit Test Cases

---
+++ JUnit/src/test/java/com/junit/helper/StringHelperTest.java
@@ -0,0 +1,32 @@
+package com.junit.helper;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class StringHelperTest {
+
+	
+	//AACD =CD ,ACDI , AAAA,ABCD
+	@Test
+	public  void testTruncateAInFirst2Positions() {
+		StringHelper helper = new StringHelper();
+		
+		
+		assertEquals("CD",helper.truncateAInFirst2Positions("AACD"));
+	//	assertEquals("CD",helper.truncateAInFirst2Positions("AACD"));
+
+	}
+
+	
+	@Test
+	public  void testTruncateAInFirst2Positions2() {
+		StringHelper helper = new StringHelper();
+		
+		
+	//	assertEquals("CD",helper.truncateAInFirst2Positions("AACD"));
+		assertEquals("",helper.truncateAInFirst2Positions("AA"));
+
+	}
+
+}
