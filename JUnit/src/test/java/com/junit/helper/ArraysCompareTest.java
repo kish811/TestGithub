adcc4b722af9e8097ed248265ce60a0b817678d7
2160731 <2160731@LTIN444067.cts.com>
2023-07-10 19:02:02 +0530

    commit

--- JUnit/src/test/java/com/junit/helper/ArraysCompareTest.java
+++ JUnit/src/test/java/com/junit/helper/ArraysCompareTest.java
@@ -5,18 +5,36 @@ import static org.junit.Assert.assertArrayEquals;
 import java.util.Arrays;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
 
 class ArraysCompareTest {
 
 	@Test
 	public void testArraysSort() {
-		int[] numbers = {12,3,4,1};
-		int[] expected = {1,3,4,12};
+		int[] numbers = { 12, 3, 4, 1 };
+		int[] expected = { 1, 3, 4, 12 };
 		Arrays.sort(numbers);
-		
-		assertArrayEquals(expected,numbers);
 
-				 		
+		assertArrayEquals(expected, numbers);
+
+	}
+
+//
+//	@test(expected=NullPointerException.class)
+//	public void testArraysSortNull() {
+//		int[] numbers = null;
+//		Arrays.sort(numbers);
+//
+//	}
+	
+	@Timeout(1000)
+	public void testSort_performance() {
+		int array[]= {12,23,4};
+		for(int i=1;i<100000;i++)
+		{
+			array[0]=i;
+			Arrays.sort(array);
+		}
 	}
 
 }
